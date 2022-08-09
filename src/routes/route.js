86e15e01f666ba9e9de1cbef5057a341f5e12468
Susheel1087 <susheelpal1087@gmail.com>
2022-08-08 22:46:34 -0700

    no

--- src/routes/route.js
+++ src/routes/route.js
@@ -10,12 +10,65 @@ router.get('/test-me', function (req, res) {
     res.send('My second ever api!')
 });
 
-router.get('/students', function (req, res){
-    let students = ['Sabiha', 'Neha', 'Akash']
-    res.send(students)
+router.get('/movie', function (req, res){
+    let movie = ["ABCD","3'Idiot","dhoom","kriss","pyar labjo me kha" ]
+    res.send(movie)
+
 })
 
-router.get('/student-details/:name', function(req, res){
+router.get('/movies/:indexNumber', function(req, res){
+    let moviesname = ["ABCD","3'Idiot","dhoom","kriss","pyar labjo me kha" ]
+     
+    let index = req.params.indexNumber;
+    res.send(moviesname[index])
+
+});
+
+
+
+    // PROBLEM 3
+
+router.get('/get-moviess/:indexNumber',function(req, res){  
+    
+    let moviesName=['rang de basanti','The shining','Lord of the rings','batman begins']
+    let index = req.params.indexNumber;
+
+     if(index > moviesName.length){
+        return res.send("use a valid index  ")
+     }else{
+    
+     res.send(moviesName[index])
+     }
+});
+     
+//PROBLEM 4
+
+router.get('/get-films/:indexNumber',function(req, res){   
+
+    let moviesName=[ {"id": 1,"name": "The Shining"}, 
+ {"id": 2,"name": "Incendies"}, 
+ {"id": 3,"name": "Rang de Basanti"},
+  {"id": 4,"name": "Finding Nemo"}]
+   let index = req.params.indexNumber
+    res.send(moviesName[index])
+});
+
+
+    // console.log("This is the request "+ JSON.stringify(requestParams))
+    // let movieName = requestParams.name
+    // console.log('Name of the student is ', movieName)
+    
+    // res.send('movie')
+
+
+module.exports = router;
+
+
+
+// JSON strigify function helps to print an entire object
+    // We can use any ways to print an object in Javascript, JSON stringify is one of them
+
+
     /*
     params is an attribute inside request that contains 
     dynamic values.
@@ -23,16 +76,3 @@ router.get('/student-details/:name', function(req, res){
     object where key is the variable defined in code 
     and value is what is sent in the request
     */
-
-    let requestParams = req.params
-
-    // JSON strigify function helps to print an entire object
-    // We can use any ways to print an object in Javascript, JSON stringify is one of them
-    console.log("This is the request "+ JSON.stringify(requestParams))
-    let studentName = requestParams.name
-    console.log('Name of the student is ', studentName)
-    
-    res.send('Dummy response')
-})
-
-module.exports = router;
